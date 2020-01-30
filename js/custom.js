8d8443d40b07b47249d5ff79eeabba4975a754d0
Justin Neighbors <justinn@collegesource.com>
2020-01-30 12:30:21 -0500

    Image Optimizations, testing lazy css loading.

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,3 @@
 $(document).ready(function() {
-  $("a:not(.local-link)")
-    .attr("target", "_blank")
-    .attr("rel", "noopener noreferrer");
+  $("a:not(.local-link)").attr("target", "_blank").attr("rel", "noopener noreferrer");
 });
