7c933f154537d2685ff35315050c2168e5f9a1a8
Justin Neighbors <justinn@collegesource.com>
2020-08-19 09:38:48 -0400

    Added Service Worker, updated manifest.

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
   $("a:not(.local-link)").attr("target", "_blank").attr("rel", "noopener noreferrer");
 });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("../sw.js").then(() => {
+      console.log("Service Worker Registered");
+    });
+  });
+}
