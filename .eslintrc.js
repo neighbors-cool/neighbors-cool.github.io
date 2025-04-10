21304d46fb74be78dad52defa910ebc649b39905
Justin Neighbors <justin@neighbors.cool>
2025-04-10 14:51:21 -0400

    updated .eslintrc.js file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,17 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'eslint:recommended',
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  rules: {
-    // Add custom rules here
-  },
+	env: {
+		browser: true,
+		es2021: true,
+		node: true, // Add this to define Node.js globals like 'module'
+	},
+	extends: [
+		'eslint:recommended',
+	],
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	rules: {
+		'no-undef': 'error', // Ensure undeclared variables are disallowed
+	},
 };
